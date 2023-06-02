c3bfb5a9ee43885d2df34c0a7a6e7213db1e2ff8
unknown <tiago@gigafull.com.br>
2023-06-02 14:53:48 -0300

    v6.9

--- servicedesk/consultar_chamados/scripts/js_smart.php
+++ servicedesk/consultar_chamados/scripts/js_smart.php
@@ -17,7 +17,11 @@
                 excluiRascunho();
                 $('#relatarChamado')[0].reset();
                 $("#basicModal").modal('hide');
-                recarregarPagina();
+                setTimeout(function() {
+                    recarregarPagina();
+                }, 2500); // Delay de 2.5 segundos (2500 milissegundos) antes de chamar recarregarPagina()
+
+
             }
         });
     });
