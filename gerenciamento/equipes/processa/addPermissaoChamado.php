cd01891ff13c05ee246b555e80915b9aa698d450
unknown <tiago@gigafull.com.br>
2024-02-27 09:53:59 -0300

    v13.2

--- gerenciamento/equipes/processa/addPermissaoChamado.php
+++ gerenciamento/equipes/processa/addPermissaoChamado.php
@@ -6,7 +6,7 @@ $idTipoChamado = $_POST['idTipoChamadoFormChamado'];
 
 $cont_insert = false;
 
-$insert_integrante = "INSERT INTO chamados_autorizados_by_equipe (equipe_id, tipo_id) VALUES (:idEquipe, :tipo_id)";
+$insert_integrante = "INSERT INTO chamados_autorizados_abertura (equipe_id, tipo_id) VALUES (:idEquipe, :tipo_id)";
 $stmt1 = $pdo->prepare($insert_integrante);
 $stmt1->bindParam(':idEquipe', $idEquipe);
 $stmt1->bindParam(':tipo_id', $idTipoChamado);
